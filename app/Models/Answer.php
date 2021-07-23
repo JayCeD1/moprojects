36868fe7c37f1ab0e8a171243b76869b52624461
JayCeD1 <jesseeren@gmail.com>
2021-07-23 23:32:27 +0300

    designing answers schema and renaming column name

---
+++ app/Models/Answer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function getBodyHtmlAttribute()
+    {
+        return Str::markdown($this->body);
+    }
+}
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -42,7 +42,7 @@ class Question extends Model
 
     public function getStatusAttribute()
     {
-        if ($this->answers > 0 ){
+        if ($this->answers_count > 0 ){
             if ($this->best_answer_id){
                 return "answered-accepted";
             }
@@ -55,6 +55,10 @@ class Question extends Model
     public function getBodyHtmlAttribute()
     {
         return Str::markdown($this->body);
+    }
 
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
     }
 }
